92deb2cb902e8cb8466dc178c8ae4a36b6d68df1
npedot <nicola.pedot@gmail.com>
2017-11-06 12:37:09 +0100

    sets gtag into index page (wip)

--- js/analytics.js
+++ js/analytics.js
@@ -1,15 +1,12 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109268425-1"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-109268425-1');
-</script>
 <!-- end Global site tag-->
 <!-- Google Analytics -->
-<script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -17,5 +14,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-109268425-1', 'auto');
 ga('send', 'pageview');
-</script>
 <!-- End Google Analytics -->
